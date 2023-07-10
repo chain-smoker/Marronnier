9b272053349c2d490d9ae088dce72b6af13840fb
JeonTaeHyeon <jth660084@gmail.com>
2023-07-10 19:25:33 +0900

    피드 컨트롤러 작성 (#8, #11, #13)

--- src/main/java/com/chainsmoker/marronnier/feed/command/application/controller/FeedController.java
+++ src/main/java/com/chainsmoker/marronnier/feed/command/application/controller/FeedController.java
@@ -1,4 +1,26 @@
 package com.chainsmoker.marronnier.feed.command.application.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/feed")
 public class FeedController {
+    @PostMapping("")
+    public String writeFeed() {
+        //피드 작성하는 코드
+        return "feed/feed";
+    }
+
+    @PostMapping("/{feedId}")
+    public String modifyFeed(@PathVariable Long feedId){
+
+
+        return "feed/detail";
+    }
+
 }
