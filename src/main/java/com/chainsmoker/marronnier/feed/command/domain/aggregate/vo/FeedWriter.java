1fffcf7b687d25cc847805e0979424077d0adf8e
JeonTaeHyeon <jth660084@gmail.com>
2023-07-10 17:13:49 +0900

    피드 엔티티 작성 (#9)

---
+++ src/main/java/com/chainsmoker/marronnier/feed/command/domain/aggregate/vo/FeedWriter.java
@@ -0,0 +1,10 @@
+package com.chainsmoker.marronnier.feed.command.domain.aggregate.vo;
+
+import javax.persistence.Column;
+
+public class FeedWriter {
+//    private Long memberId;
+//    private String memberName;
+
+
+}
