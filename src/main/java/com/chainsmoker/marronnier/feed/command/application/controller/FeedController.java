18ae082e218912feb95631e64826a78aa51d6047
JeonTaeHyeon <jth660084@gmail.com>
2023-07-10 19:18:03 +0900

    피드 컨트롤러 작성 (#8, #11, #13)

--- src/main/java/com/chainsmoker/marronnier/feed/command/application/controller/FeedController.java
+++ src/main/java/com/chainsmoker/marronnier/feed/command/application/controller/FeedController.java
@@ -1,4 +1,24 @@
 package com.chainsmoker.marronnier.feed.command.application.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class FeedController {
+    @PostMapping("/feed")
+    public String writeFeed() {
+        //피드 작성하는 코드
+        return "feed/feed";
+    }
+
+    @PostMapping("/feed/{feedId")
+    public String modifyFeed(@PathVariable Long feedId){
+
+
+        return "feed/detail";
+    }
+
 }
--- src/main/java/com/chainsmoker/marronnier/feed/query/application/controller/FeedQController.java
+++ src/main/java/com/chainsmoker/marronnier/feed/query/application/controller/FeedQController.java
@@ -1,4 +1,34 @@
 package com.chainsmoker.marronnier.feed.query.application.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class FeedQController {
+    @GetMapping("/feed")
+    public String viewAllFeed() {
+        //모든 피드를 보여주는 코드 추가
+        return "feed/feed";
+    }
+
+    @GetMapping("/feed/write")
+    public String moveToFeedWrite() {
+        //피드 작성하는 페이지로 이동
+        //근데 피드는 여기서 작성하는게 맞았나?
+        return "feed/write";
+    }
+
+    @GetMapping("/feed/{feedId}")
+    public String feedDetail(@PathVariable Long feedId) {
+        //pathvariable에 해당하는 피드 보는 페이지
+
+
+        return "feed/detail";
+    }
+
 }
