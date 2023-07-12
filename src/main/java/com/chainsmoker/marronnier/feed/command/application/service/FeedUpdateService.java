d70761937ae543f1b733cc34f8c2afda411b8389
JeonTaeHyeon <jth660084@gmail.com>
2023-07-12 18:20:28 +0900

    파일 누락 긴급 수정 [Closes #67]

---
+++ src/main/java/com/chainsmoker/marronnier/feed/command/application/service/FeedUpdateService.java
@@ -0,0 +1,23 @@
+package com.chainsmoker.marronnier.feed.command.application.service;
+
+import com.chainsmoker.marronnier.feed.command.application.dto.UpdateFeedDTO;
+import com.chainsmoker.marronnier.feed.command.domain.aggregate.entity.Feed;
+import com.chainsmoker.marronnier.feed.command.domain.repository.FeedReposiroty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedUpdateService {
+    private final FeedReposiroty feedReposiroty;
+
+    @Autowired
+    public FeedUpdateService(FeedReposiroty feedReposiroty) {
+        this.feedReposiroty = feedReposiroty;
+    }
+
+    public void updateFeed(UpdateFeedDTO updateFeedDTO){
+        Feed feed = new Feed(updateFeedDTO);
+        feedReposiroty.save(feed);
+    }
+
+}
