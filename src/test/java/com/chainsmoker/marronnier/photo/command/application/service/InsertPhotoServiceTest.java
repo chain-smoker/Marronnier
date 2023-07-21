6422b04470c39c6c7a8b126c18b3103a1e2d0528
myeonghwan cho <1jmhcho@gmail.com>
2023-07-21 14:39:37 +0900

    [#94] 사진 업로드(통합 테스트 코드 적용)

--- src/test/java/com/chainsmoker/marronnier/photo/command/application/service/InsertPhotoServiceTest.java
+++ src/test/java/com/chainsmoker/marronnier/photo/command/application/service/InsertPhotoServiceTest.java
@@ -1,6 +1,7 @@
 package com.chainsmoker.marronnier.photo.command.application.service;
 
 import com.chainsmoker.marronnier.photo.command.application.dto.PhotoDTO;
+import com.chainsmoker.marronnier.photo.command.domain.aggregate.entity.PhotoCategory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.stream.Stream;
 
+import static com.chainsmoker.marronnier.photo.command.domain.aggregate.entity.PhotoCategory.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /* 통합 테스트 */
@@ -24,24 +26,21 @@ class InsertPhotoServiceTest {
 
         return Stream.of(
                 Arguments.of(
-                        1L,
                         "오리지널이름",
                         "변경후이름",
-                        "사진구분",
+                        COCKTAIL_RECIPE,
                         "사진위치"
                 ),
                 Arguments.of(
-                        2L,
                         "오리지널이름2",
                         "변경후이름2",
-                        "사진구분2",
+                        ELEMENT,
                         "사진위치2"
                 ),
                 Arguments.of(
-                        3L,
                         "오리지널이름3",
                         "변경후이름3",
-                        "사진구분3",
+                        FEED,
                         "사진위치3"
                 )
         );
@@ -50,10 +49,9 @@ class InsertPhotoServiceTest {
     @DisplayName("사진 저장하기 테스트")
     @ParameterizedTest
     @MethodSource("createPhoto")
-    void testCreateTable(long photoId, String photoName, String photoRename, String photoCategory, String photoRoot) {
+    void testCreateTable(String photoName, String photoRename, PhotoCategory photoCategory, String photoRoot) {
 
         PhotoDTO photoInfo = new PhotoDTO(
-                photoId,
                 photoName,
                 photoRename,
                 photoCategory,
