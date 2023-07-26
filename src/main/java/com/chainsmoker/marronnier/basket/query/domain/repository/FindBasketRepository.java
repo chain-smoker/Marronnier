ce622ee44a0fd5ef91af10003edc4efe031f3cd1
jeongjiwon <jeewonre@hufs.ac.kr>
2023-07-26 17:07:41 +0900

    [#167] 내가 좋아요 누른 피드 조회

--- src/main/java/com/chainsmoker/marronnier/basket/query/domain/repository/FindBasketRepository.java
+++ src/main/java/com/chainsmoker/marronnier/basket/query/domain/repository/FindBasketRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FindBasketRepository extends JpaRepository<QueryBasket, Long> {
 
     Page<QueryBasket> findAllByBasketCompositeKey_MemberId(long memberId, Pageable pageable);
