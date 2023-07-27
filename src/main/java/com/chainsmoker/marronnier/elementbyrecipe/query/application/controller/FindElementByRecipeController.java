16efd4c787d0eb25dc6cecce6917cf82045315dd
CodeJugller <jym7546@gmail.com>
2023-07-27 14:25:13 +0900

    [#142] 칵테일 상세보기 페이지 조주법 추가

--- src/main/java/com/chainsmoker/marronnier/elementbyrecipe/query/application/controller/FindElementByRecipeController.java
+++ src/main/java/com/chainsmoker/marronnier/elementbyrecipe/query/application/controller/FindElementByRecipeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -28,15 +29,30 @@ public class FindElementByRecipeController {
     }
 
     @GetMapping("find/elementbyrecipe/{recipeId}")
-    public String findElementByRecipe(CocktailRecipeDTO recipeDTO, @PathVariable Long recipeId, Model model, Authentication authentication) {
+    public String findElementByRecipe(CocktailRecipeDTO cocktailRecipeDTO, @PathVariable Long recipeId, Model model, Authentication authentication) {
         // 수정  현재 사용한 FindElementDTO는 Element Package에서 구현된 것
         // 수정  이걸 ElementByRecipe에서 따로 DTO를 만들어서 할까??
-        if (recipeDTO.getName() == null) {
-            FindCocktailRecipeDTO recipe = findElementByRecipeService.findRecipeById(recipeId);
-            model.addAttribute("recipe", recipe);
+        if (cocktailRecipeDTO.getName() == null) {
+            FindCocktailRecipeDTO cocktailRecipe = findElementByRecipeService.findRecipeById(recipeId);
+            String[] recipes=cocktailRecipe.getRecipe().split("-");
+            int recipesSize=recipes.length;
+            List<String> recipe=new ArrayList<>();
+            for (int i=0; i< recipesSize; i++){
+                recipe.add(recipes[i]);
+            }
+            model.addAttribute("recipes",recipe);
+            model.addAttribute("cocktailRecipe", cocktailRecipe);
         } else {
-            model.addAttribute("recipe", recipeDTO);
+            String[] recipes=cocktailRecipeDTO.getRecipe().split("-");
+            int recipesSize=recipes.length;
+            List<String> recipe=new ArrayList<>();
+            for (int i=0; i< recipesSize; i++){
+                recipe.add(recipes[i]);
+            }
+            model.addAttribute("s",recipe);
+            model.addAttribute("cocktailRecipe", cocktailRecipeDTO);
         }
+        
         SessionUser sessionUser = (SessionUser) authentication.getPrincipal();
         boolean memberIsAuthenticated = authentication.isAuthenticated();
         model.addAttribute("member", sessionUser);
