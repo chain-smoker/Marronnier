dab1b121edcac9be786f81d8f0a2abc57f0781df
jeongjiwon <jeewonre@hufs.ac.kr>
2023-07-29 12:26:33 +0900

    [Closes #209] 칵테일 레시피 조회 테스트 코드 작성

--- src/test/java/com/chainsmoker/marronnier/cocktail/CocktailRecipeTests.java
+++ src/test/java/com/chainsmoker/marronnier/cocktail/CocktailRecipeTests.java
@@ -73,6 +73,7 @@ public class CocktailRecipeTests {
         recipeDTO.setDifficulty(difficulty);
 
         CocktailRecipe registCocktailRecipe = registCocktailRecipeService.regist(recipeDTO);
+        System.out.println("registCocktailRecipe Id = " + registCocktailRecipe.getId());
         FindCocktailRecipeDTO cocktail = findCocktailRecipeService.findByCocktailRecipeId(registCocktailRecipe.getId());
         Assertions.assertNotNull(cocktail.getRecipe());
     }
